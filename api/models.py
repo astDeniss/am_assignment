105e187d30f81b024e1aa7a3e7ad0d00e68c6221
Dennis Ast <astasitsev@gmail.com>
2020-06-20 10:16:58 +0300

    models, serializers created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Categories(models.Model):
+    category = models.OneToOneField(Category, on_delete=models.CASCADE, related_name='category_obj')
+    children = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.category.name
+
+    def create_profile(sender, **kwargs):
+        category = kwargs["instance"]
+        if kwargs["created"]:
+            category_profile = Categories(category=category)
+            category_profile.save()
+
+    post_save.connect(create_profile, sender=Category)
